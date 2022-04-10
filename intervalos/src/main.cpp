a75e2b1bc9c0b175e11567bc0894fb58eabd7715
Nashin <38250121+erednasyl@users.noreply.github.com>
2022-04-10 20:29:38 -0300

    Done

--- intervalos/src/main.cpp
+++ intervalos/src/main.cpp
@@ -1,9 +1,3 @@
-/*!
- * @brief This code implements the Intervalos programming problem
- * @author selan
- * @data June, 6th 2021
- */
-
 #include <iostream>
 using std::cout;
 using std::cin;
@@ -11,11 +5,42 @@ using std::endl;
 #include <iomanip>
 using std::setprecision;
 
-// Se desejar, crie funções aqui, antes do main().
-
 int main(void)
 {
-    // TODO: Adicione aqui a sua solução.
+    int x;
+    int numQnt = 0;
+    float in025 = 0, in2550 = 0, in5075 = 0, in75100 = 0, out = 0;
+
+    while( cin >> std::ws >> x) {
+        if (x >= 0 && x < 25){
+            in025++;
+        }
+
+        else if (x >= 25 && x < 50){
+            in2550++;
+        }
+
+        else if(x >= 50 && x < 75){
+            in5075++;
+        }
+
+        else if(x >= 75 && x < 100){
+            in75100++;
+        }
+
+        else{
+            out++;
+        }
+
+        numQnt++;
+    }
+
+
+    cout << std::setprecision(4) << (in025/numQnt)*100 << endl;
+    cout << std::setprecision(4) << (in2550/numQnt)*100 << endl;
+    cout << std::setprecision(4) << (in5075/numQnt)*100 << endl;
+    cout << std::setprecision(4) << (in75100/numQnt)*100 << endl;
+    cout << std::setprecision(4) << (out/numQnt)*100 << endl;
 
     return 0;
 }
