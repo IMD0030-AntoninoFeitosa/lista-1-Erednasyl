81f74977fcd7da3a6803db7434686eed126860ff
Nashin <38250121+erednasyl@users.noreply.github.com>
2022-04-11 01:39:13 -0300

    Done

--- ponto_em_retangulo_2/src/main.cpp
+++ ponto_em_retangulo_2/src/main.cpp
@@ -1,9 +1,3 @@
-/*!
- * @brief Implementação do Ponto em Retângulo V2.
- * @author selan
- * @data June, 6th 2021
- */
-
 #include <iostream>
 using std::cout;
 using std::cin;
@@ -14,14 +8,17 @@ using std::max;
 
 #include "function.h"
 
-//=== Funções
-
-// Coloque aqui qualquer função auxiliar que desejar.
-
-
 int main(void)
 {
-    // TODO: Adicione aqui seu código.
+    Ponto p1, p2, p3;
+    while( cin >> std::ws >> p1.x >> p1.y >> p2.x >> p2.y >> p3.x >> p3.y) {
+        location_t result = function::pt_in_rect(p1, p2, p3);
+
+        if (result == INVALID) cout << "invalid" << endl;
+        if (result == BORDER) cout << "border" << endl;
+        if (result == INSIDE) cout << "inside" << endl;
+        if (result == OUTSIDE) cout << "outside" << endl;
+    }
 
     return 0;
 }
