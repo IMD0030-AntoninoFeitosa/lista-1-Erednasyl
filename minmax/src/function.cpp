4b1a26031a53dd9e90ecdc5032becf8c25345d3e
Nashin <38250121+erednasyl@users.noreply.github.com>
2022-04-11 20:30:11 -0300

    Done

--- minmax/src/function.cpp
+++ minmax/src/function.cpp
@@ -1,19 +1,23 @@
 #include "function.h"
-
-/*! 
- * Finds and returns a pair with the first instance of the smallest element
- * and the last instance of the largest element in an array.
- *
- * @param V This is the array-to-pointer decay representing an array.
- * @param n The array's size.
- *
- * @return A pair of indexes to the first smallest and last largest values.
- */
-
 std::pair<int,int> min_max( int V[], size_t n )
 {
-    // TODO: Adicione aqui sua solução.
+    int max = 0;
+    int min = INT_MAX;
+    int maxIndex = -1;
+    int minIndex = -1;
 
-    // TODO: Isso é apenas um STUB. Substitua com seu retorno correto.
-    return { -1, -1 };
+    for (size_t i = 0; i < n; i++)
+    {
+        if (V[i] >= max){
+            max = V[i];
+            maxIndex = i;
+        }
+        if (V[i] < min)
+        {
+            min = V[i];
+            minIndex = i;
+        }
+    }
+
+    return {minIndex, maxIndex};
 }
