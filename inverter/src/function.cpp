d29d7988444b16a0ee29dee4adfaf3708a2bac97
Nashin <38250121+erednasyl@users.noreply.github.com>
2022-04-11 21:00:23 -0300

    Done

--- inverter/src/function.cpp
+++ inverter/src/function.cpp
@@ -1,12 +1,18 @@
 #include <iostream>
 #include <array>
-
-/*! 
- * Reverse de order of elements inside the array.
- * @param arr Reference to the array with the values.
- */
+ 
 template <std::size_t SIZE>
 void reverse( std::array< std::string, SIZE > & arr )
 {
-    /*codigo*/
+    std::string temp;
+    int iterator;
+
+    for (size_t i = 0; i < (arr.size()/2); i++)
+    {
+        iterator = arr.size() - i - 1;
+        temp = arr[i];
+        arr[i] = arr[iterator];
+        arr[iterator] = temp;
+    }
+    
 }
