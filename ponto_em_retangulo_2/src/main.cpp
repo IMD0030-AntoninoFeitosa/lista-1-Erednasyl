a78ca39c00a47b7da14e3f209094daaf3399396a
Giselle Neres <38250121+erednasyl@users.noreply.github.com>
2022-04-30 01:31:55 -0300

    Done, confusion happened xD

--- ponto_em_retangulo_2/src/main.cpp
+++ ponto_em_retangulo_2/src/main.cpp
@@ -1,144 +1,37 @@
+@@ -1,9 +1,3 @@
+/*!
+ * @brief Implementação do Ponto em Retângulo V2.
+ * @author selan
+ * @data June, 6th 2021
+ */
+
 #include <iostream>
-#include <array>
-#include <vector>
-#include <string>
-#include <fstream>
-#include <cstdlib>
+using std::cout;
+using std::cin;
+using std::endl;
+#include <algorithm>
+using std::min;
+using std::max;
 
-#include "Game.h"
+#include "function.h"
 
-const std::string CONFIG_FILE = "config.cfg";
+//=== Funções
 
-void show_usage(void){
-  std::cout << "Usage: game [option]" << std::endl;
-  std::cout << "Option:" << std::endl;
-  std::cout << " -h or --help                  Display this information." << std::endl;
-  std::cout << " -d or --difficulty <option>   Change the game difficulty for <option>" << std::endl;
-  std::cout << "                               -b or --beginner" << std::endl;
-  std::cout << "                               -i or --intermediary" << std::endl;
-  std::cout << "                               -a or --advanced" << std::endl;
-}
-
-void show_leaderboard(){
-  std::cout << "LEADERBOARD --- BEGINNER" << std::endl;
-  for (int i = 0; i < 20; i++){
-    std::cout << i+1 << " --- FULANINHOOSSSSS" << std::endl;
-  }
-}
-
-void start_game(Difficulty level){
-  unsigned long seed = 0;
-  std::srand(seed);
-  
-  Game game = create_game(level);
-  //MOSTRAR INSTRUÇÕES AO JOGADOR
-  Map map = create_map(game);
-  
-  show_map(game, map);
-
-  for (int i = 0; i < 10; i++){
-    short x,y;
-    std::cin >> x >> y;
-    map[x][y].is_hidden = false;
-    show_map(game, map);
-    if (map[x][y].has_bomb == true){
-      end_game(true);
-      break;
-    }
-  }
-
-  
-  
-}
-
-void store_difficulty(const std::string config_file, Difficulty level){
-  std::ofstream file;
-  file.open (config_file.c_str(), std::ifstream::out);
-  if(file.is_open()){
-    switch(level){
-      case Difficulty::beginner:
-        file << 'b';
-        std::cout << "Difficulty set to begginer!" << std::endl;
-        break;
-      case Difficulty::intermediary:
-        file << 'i';
-        std::cout << "Difficulty set to intermediary!" << std::endl;
-        break;
-      case Difficulty::advanced:
-        file << 'a';
-        std::cout << "Difficulty set to advanced!" << std::endl;
-        break;
-    }
-    file.close();
-  }
-}
+// Coloque aqui qualquer função auxiliar que desejar.
 
 
-Difficulty load_difficulty(const std::string config_file){
-  Difficulty level;
-  std::ifstream file;
-  file.open (config_file.c_str(), std::ifstream::in);
-  if(file.is_open()){
-    char c;
-    file >> c;
-    switch(c){
-      case 'b': level = Difficulty::beginner; break;
-      case 'i': level = Difficulty::intermediary; break;
-      case 'a': level = Difficulty::advanced; break;
-    }
-    file.close();
-  } else {
-    store_difficulty(config_file, Difficulty::beginner);
-    level = Difficulty::beginner;
-  }
-  return level;
-}
+int main(void)
+{
+    // TODO: Adicione aqui seu código.
+    Ponto p1, p2, p3;
+    while( cin >> std::ws >> p1.x >> p1.y >> p2.x >> p2.y >> p3.x >> p3.y) {
+        location_t result = function::pt_in_rect(p1, p2, p3);
 
-
-int main(int argc, char** argv){
-  if (argc > 1){
-    std::string arg = argv[1];
-    if (arg == "-h" || arg == "-help"){
-      show_usage();
+        if (result == INVALID) cout << "invalid" << endl;
+        if (result == BORDER) cout << "border" << endl;
+        if (result == INSIDE) cout << "inside" << endl;
+        if (result == OUTSIDE) cout << "outside" << endl;
     }
 
-    else if (arg == "-r" || arg == "--records"){
-      show_leaderboard();
-    }
-
-    else if (arg == "-d" || arg == "--difficulty"){
-      if (argc > 2){
-
-        //PARA FAZER COMPARAÇÕES É NECESSÁRIO CRIAR UMA STRING COM O VALOR DE ARGV[i]
-        std::string newlevel = argv[2];
-        
-        if(newlevel == "-b" || newlevel == "--beginner"){
-          store_difficulty(CONFIG_FILE, Difficulty::beginner);
-        }
-        else if(newlevel == "-i" || newlevel == "--intermediary"){
-          store_difficulty(CONFIG_FILE, Difficulty::intermediary);
-        }
-        else if(newlevel == "-a" || newlevel == "--advanced"){
-          store_difficulty(CONFIG_FILE, Difficulty::advanced);
-        }
-        else {
-          std::cout << "Unknown difficulty argument: " << newlevel << std::endl;
-          show_usage();
-        }
-      }
-      else {
-        std::cout << "It was expected a difficulty for: " << argv[1] << std::endl;
-        show_usage();
-      }
-    }
-    else {
-      std::cout << "Unknown argument: " << argv[1] << std::endl;
-      show_usage();
-    }
-  }
-  else {
-    Difficulty level = load_difficulty(CONFIG_FILE);
-    start_game(level);
-  }
-  return 0;
+    return 0;
 }
